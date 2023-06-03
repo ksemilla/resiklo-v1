f91ba7cd5cb6363796ce963678d4f47340bc1e7d
Kevin Semilla <kevinsemilla@gmail.com>
2023-06-04 00:25:54 +1000

    home page placeholders

--- src/app/Nav.tsx
+++ src/app/Nav.tsx
@@ -1,14 +1,40 @@
+"use client"
+
 import Image from "next/image"
+import { useEffect, useState } from "react"
+
+const classNames = (...classes: string[]) => {
+  return classes.filter(Boolean).join(" ")
+}
 
 const Nav = () => {
+  const [scrollPosition, setScrollPosition] = useState(0)
+
+  const handleScroll = () => {
+    const position = window.pageYOffset
+    setScrollPosition(Math.floor(position / 20))
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
   return (
-    <nav className="p-4 sticky top-0 flex justify-between items-center">
+    <nav
+      className={classNames(
+        "p-4 sticky top-0 flex justify-between items-center bg-white opacity-90 z-50",
+        scrollPosition > 0 ? "shadow-sm" : ""
+      )}
+    >
       <div>
         <Image
           src="/resiklo.png"
           width={180}
           height={100}
           alt="resiklo-lettering"
+          priority
         />
       </div>
       <div className="flex items-center space-x-10">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
   const handleScroll = () => {
     const position = window.pageYOffset
-    setScrollPosition(Math.floor(position / 20))
+    setScrollPosition(Math.floor(position / 15))
   }
 
   useEffect(() => {
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <main>
       <i
-        className="fa-solid fa-gear fixed text-gray-600 text-opacity-30"
+        className="fa-solid fa-gear fixed text-gray-600 text-opacity-20 z-0"
         style={{
           transform: `rotate(${scrollPosition}deg)`,
           fontSize: "1200px",
@@ -26,11 +26,15 @@ export default function Home() {
           right: "-600px",
         }}
       ></i>
-      <div className="max-w-5xl m-auto">
+      <div className="max-w-5xl m-auto space-y-72 mb-72">
         <div className="text-center py-20">
           <Hero />
         </div>
+        <div>
+          <Products />
+        </div>
       </div>
+      <Footer />
     </main>
   )
 }
@@ -73,5 +77,41 @@ const Hero = () => {
       </h1>
       <span className="blinker text-3xl">{text}</span>
     </section>
+  )
+}
+
+const Products = () => {
+  return (
+    <section id="products" className="max-w-3xl m-auto">
+      <div className="grid grid-cols-3 gap-4">
+        <div className="col-span-1 flex flex-col space-y-4 pt-20">
+          <div className="bg-gray-50 w-full h-56 z-10" />
+          <div className="bg-gray-50 w-full h-56 z-10" />
+        </div>
+        <div className="col-span-1 flex flex-col space-y-4 pt-32">
+          <div className="bg-gray-50 w-full h-56 z-10" />
+          <div className="bg-gray-50 w-full h-56 z-10" />
+        </div>
+        <div className="col-span-1 flex flex-col space-y-4">
+          <div className="bg-gray-50 w-full h-56 z-10" />
+          <div className="bg-gray-50 w-full h-56 z-10" />
+          <div className="bg-gray-50 w-full h-56 z-10" />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+const Footer = () => {
+  return (
+    <footer
+      style={{ minHeight: "300px" }}
+      className="bg-black text-white opacity-80"
+    >
+      <div className="text-center pt-24">
+        Copyright <i className="fa-regular fa-copyright"></i> 2021 Resiklo
+        Machine Shop. All Rights Reserved.
+      </div>
+    </footer>
   )
 }
